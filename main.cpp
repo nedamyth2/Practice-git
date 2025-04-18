2d0e1dcf0e594f4e0089551b21ada9a08c9eedb7
nedamyth <nedamyth2@gmail.com>
2025-04-18 16:18:31 +0330

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void printStudentID() {
+    cout << "my student ID: 4033623004" << endl;
+}
+
+
 int main() {
     cout << "Hello, World" << endl;
+    printStudentID();
+
     return 0;
 }
