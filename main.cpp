55791771327fd827d23eb718be441fbb6e9654b4
nedamyth <nedamyth2@gmail.com>
2025-04-18 16:23:53 +0330

    Revert "Second Commit"
    This reverts commit 2d0e1dcf0e594f4e0089551b21ada9a08c9eedb7.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void printStudentID() {
-    cout << "my student ID: 4033623004" << endl;
-}
-
-
 int main() {
     cout << "Hello, World" << endl;
-    printStudentID();
-
     return 0;
 }
